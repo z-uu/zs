cd1243ae2c946b796b4d75170ca3a51032e6f2b4
ZackaryW <gitzackw@gmail.com>
2025-02-24 11:29:28 -0800

    feat: zs core cmds

--- src/zs/core/__init__.py
+++ src/zs/core/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import json
 ZS_EXE_PATH = os.path.dirname(sys.executable)
 APP_CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".zs")
 
@@ -11,9 +11,14 @@ def get_installed():
         if not exep.endswith(".exe"):
             continue
 
+        if exep in ["zs.kvstore.exe"]:
+            continue
+
         if not exep.startswith("zs."):
             continue
 
-        yield exep
+        yield exep.replace(".exe", "")
 
 INSTALLED = list(get_installed())
+
+INDEX = json.load(open(os.path.join(os.path.dirname(__file__), "index.json")))
--- src/zs/core/__main__.py
+++ src/zs/core/__main__.py
@@ -1,8 +1,8 @@
-
 import os
 import sys
 import click
 import zs.core
+import subprocess
 
 @click.group()
 def cli():
@@ -19,7 +19,47 @@ def list():
 @cli.command()
 @click.argument("exe", type=str)
 def install(exe):
-    os.system(f"{sys.executable} -m pip install ")
+    if exe in zs.core.INSTALLED:
+        click.echo(f"Already installed: {exe}")
+        return
+    if exe not in zs.core.INDEX:
+        click.echo(f"Not found: {exe}")
+        return
+    
+    git_url = zs.core.INDEX[exe]["git_url"].split("://")[1]
+    git_url = "https://" + git_url  
+    os.system(f"{sys.executable} -m pip install git+{git_url}")
+
+@cli.command()
+@click.argument("exe", type=str)
+def uninstall(exe):
+    if exe not in zs.core.INSTALLED:
+        click.echo(f"Not installed: {exe}")
+        return
+    os.system(f"{sys.executable} -m pip uninstall {exe}")
+
+@cli.command()
+@click.argument("exe", type=str, required=False)
+@click.option("--all", is_flag=True)
+def update(exe, all):
+    if not all and not exe:
+        click.echo("Please provide either an executable name or --all flag")
+        return
+    
+    if all:
+        # update zs first
+        subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "zs"])
+
+        for exe in zs.core.INSTALLED:
+            if exe == "zs":
+                continue
+            subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", exe])
+        return
+    
+    if exe not in zs.core.INSTALLED:
+        click.echo(f"Not installed: {exe}")
+        return
+    subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", exe])
 
 if __name__ == "__main__":
     cli()
