96a06e1285897f18ec2229e95ccdc8eadb84f839
ZackaryW <gitzackw@gmail.com>
2025-02-24 11:46:12 -0800

    fix: as prev

--- src/zs/core/__main__.py
+++ src/zs/core/__main__.py
@@ -46,17 +46,11 @@ def update(exe, all):
         click.echo("Please provide either an executable name or --all flag")
         return
     
-    if exe == "zs":
+    if exe == "zs" or all:
         subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", "git+https://github.com/z-uu/zs.git"])
         return
 
     if all:
-        # Update zs first using git_url
-        if "zs" in zs.core.INDEX:
-            git_url = zs.core.INDEX["zs"]["git_url"].split("://")[1]
-            git_url = "https://" + git_url
-            subprocess.call([sys.executable, "-m", "pip", "install", "--upgrade", f"git+{git_url}"])
-        
         for exe in zs.core.INSTALLED:
             if exe == "zs":
                 continue
